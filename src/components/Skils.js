10493709c65a81ce4fd7e1644d58cd65b32b72fe
aniketjha1729 <kumaraniket1729@gmail.com>
2021-11-22 15:44:38 +0530

    badge change

--- src/components/Skils.js
+++ src/components/Skils.js
@@ -1,189 +1,109 @@
 import React from "react";
 import "./skills.css";
+import { Doughnut, Bar, Line } from "react-chartjs-2";
+
 const Skils = () => {
   return (
     <div className="skillContainer container">
       <div className="skillHeading">
         <b>
           <u>Skills & Technology</u>
-        </b>{" "}
+        </b>
       </div>
-      <div className="badgeContainer">
-        <div className="box">
-          <div className="percent">
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle
-                cx="70"
-                cy="70"
-                r="70"
-                style={{ strokeDashoffset: "calc(440 - (440 * 40) / 100)" }}
-              ></circle>
-            </svg>
-            <div className="number" style={{ color: "aliceblue" }}>
-              <h2>40%</h2>
-            </div>
+      <div className="donutContainer">
+        <div className="donut">
+          <div>
+            <Doughnut
+              data={{
+                datasets: [
+                  {
+                    data: [40, 60],
+                    backgroundColor: ["rgb(240,248,255)", "rgb(40,76,91)"],
+                    borderColor: ["rgba(0, 0, 0, 1)", "rgba(0,0,0,1)"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+            />
           </div>
-          <h2 className="text" style={{ color: "aliceblue" }}>
-            SpringBoot
-          </h2>
-        </div>
-        <div className="box">
-          <div className="percent">
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle
-                cx="70"
-                cy="70"
-                r="70"
-                style={{ strokeDashoffset: "calc(440 - (440 * 85) / 100)" }}
-              ></circle>
-            </svg>
-            <div className="number" style={{ color: "aliceblue" }}>
-              <h2>85%</h2>
-            </div>
-          </div>
-          <h2 className="text" style={{ color: "aliceblue" }}>
-            NodeJS
-          </h2>
-        </div>
-        <div className="box">
-          <div className="percent">
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle
-                cx="70"
-                cy="70"
-                r="70"
-                style={{ strokeDashoffset: "calc(440 - (440 * 80) / 100)" }}
-              ></circle>
-            </svg>
-            <div className="number" style={{ color: "aliceblue" }}>
-              <h2>80%</h2>
-            </div>
-          </div>
-          <h2 className="text" style={{ color: "aliceblue" }}>
-            ReactJs
-          </h2>
-        </div>
-        <div className="box">
-          <div className="percent">
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle
-                cx="70"
-                cy="70"
-                r="70"
-                style={{ strokeDashoffset: "calc(440 - (440 * 57) / 100)" }}
-              ></circle>
-            </svg>
-            <div className="number" style={{ color: "aliceblue" }}>
-              <h2>57%</h2>
-            </div>
-          </div>
-          <h2 className="text" style={{ color: "aliceblue" }}>
-            Python
-          </h2>
-        </div>
-        <div className="box">
-          <div className="percent">
-            <svg>
-              <circle cx="70" cy="70" r="70"></circle>
-              <circle
-                cx="70"
-                cy="70"
-                r="70"
-                style={{ strokeDashoffset: "calc(440 - (440 * 87) / 100)" }}
-              ></circle>
-            </svg>
-            <div className="number" style={{ color: "aliceblue" }}>
-              <h2>87%</h2>
-            </div>
-          </div>
-          <h2 className="text" style={{ color: "aliceblue" }}>
-            Javascript
-          </h2>
-        </div>
-      </div>
-      <div className="mobilebadgeContainer">
-        <label htmlFor="">SpringBoot</label>
-        <div
-          className="progress"
-          style={{ height: "40px", marginBottom: "15px" }}
-        >
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: "40%" }}
-            aria-valuenow="40"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            45%
+          <div className="text-center">
+            <b>Springboot</b>
           </div>
         </div>
-        <label htmlFor="">NodeJs</label>
-        <div
-          className="progress"
-          style={{ height: "40px", marginBottom: "15px" }}
-        >
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: "85%" }}
-            aria-valuenow="85"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            85%
+        <div className="donut">
+          <div>
+            <Doughnut
+              data={{
+                datasets: [
+                  {
+                    data: [85, 15],
+                    backgroundColor: ["rgb(240,248,255)", "rgb(37,68,80)"],
+                    borderColor: ["rgba(0, 0, 0, 1)", "rgba(0,0,0,1)"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+            />
+          </div>
+          <div className="text-center">
+            <b>NodeJs</b>
           </div>
         </div>
-        <label htmlFor="">ReactJs</label>
-        <div
-          className="progress"
-          style={{ height: "40px", marginBottom: "15px" }}
-        >
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: "80%" }}
-            aria-valuenow="80"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            80%
+        <div className="donut">
+          <div>
+            <Doughnut
+              data={{
+                datasets: [
+                  {
+                    data: [80, 20],
+                    backgroundColor: ["rgb(240,248,255)", "rgb(33,60,70)"],
+                    borderColor: ["rgba(0, 0, 0, 1)", "rgba(0,0,0,1)"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+            />
+          </div>
+          <div className="text-center">
+            <b>ReactJs</b>
           </div>
         </div>
-        <label htmlFor="">Python</label>
-        <div
-          className="progress"
-          style={{ height: "40px", marginBottom: "15px" }}
-        >
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: "57%" }}
-            aria-valuenow="57"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            57%
+        <div className="donut">
+          <div>
+            <Doughnut
+              data={{
+                datasets: [
+                  {
+                    data: [57, 43],
+                    backgroundColor: ["rgb(240,248,255)", "rgb(28,52,60)"],
+                    borderColor: ["rgba(0, 0, 0, 1)", "rgba(0,0,0,1)"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+            />
+          </div>
+          <div className="text-center">
+            <b>Python</b>
           </div>
         </div>
-        <label htmlFor="">Javascript</label>
-        <div
-          className="progress"
-          style={{ height: "40px", marginBottom: "15px" }}
-        >
-          <div
-            className="progress-bar"
-            role="progressbar"
-            style={{ width: "87%" }}
-            aria-valuenow="87"
-            aria-valuemin="0"
-            aria-valuemax="100"
-          >
-            87%
+        <div className="donut">
+          <div>
+            <Doughnut
+              data={{
+                datasets: [
+                  {
+                    data: [87, 13],
+                    backgroundColor: ["rgb(240,248,255)", "rgb(22,43,51)"],
+                    borderColor: ["rgba(0, 0, 0, 1)", "rgba(0,0,0,1)"],
+                    borderWidth: 2,
+                  },
+                ],
+              }}
+            />
+          </div>
+          <div className="text-center">
+            <b>Javascript</b>
           </div>
         </div>
       </div>
